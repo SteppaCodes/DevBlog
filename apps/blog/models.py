f471fd257bccedf92b533010ee79a92190996dff
Big Steppa <steppacodes@gmail.com>
2023-10-22 22:45:30 -0700

    seup database, template and views

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'Draft', 'Draft'
+        PUBLISHED = 'Published', 'Published'
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name=
+                               'blog_posts')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250)
+    body = models.TextField()
+    status = models.CharField(choices=Status.choices, default=Status.DRAFT,max_length=10)
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    objects = models.Manager() #Always add the default manager when you create a custom one
+    published = PublishedManager()
+
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish'])
+        ]
+
+    def __str__(self):
+        return self.title
+    
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
+def posts(request):
+    posts = Post.published.all()
+    print(posts)
+
+    context = {
+        posts:"posts",
+    }
+    
+    return render(request,'devblog/posts/list.html',context)
+
+def post_detail(request, id):
+    post = get_object_or_404(Post, id=id, 
+                             status=Post.Status.PUBLISHED)
+
+    context= {
+        post: 'post'
+    }
+    return render(request,'devblog/posts/detail.html',context)
