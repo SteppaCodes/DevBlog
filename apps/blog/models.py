943634cac6a5cc85c1be1287419ea9340bc0aa08
Big Steppa <steppacodes@gmail.com>
2023-10-24 17:21:48 -0700

    canonical urls

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class PublishedManager(models.Manager):
@@ -31,6 +32,9 @@ class Post(models.Model):
         indexes = [
             models.Index(fields=['-publish'])
         ]
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
 
     def __str__(self):
         return self.title
--- devblog/urls.py
+++ devblog/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('devblog/', include('apps.blog.urls',))
+    path('admin/', admin.site.urls),         
+    path('devblog/', include(('apps.blog.urls', 'blog'), namespace='blog'))
 ]
+                       
