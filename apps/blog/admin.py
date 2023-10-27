c213b371809bb00026aa67649d505ec7df4becf4
Big Steppa <steppacodes@gmail.com>
2023-10-27 13:10:24 -0700

    tag-feature 1.0

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from apps.blog.models import Post, Comment
+from apps.blog.models import Post, Comment,Tag
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Comment)
+admin.site.register(Tag)
